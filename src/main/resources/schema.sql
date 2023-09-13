0ea723f2bd263e6bdd97bfbabb9115aeacb5f819
Anderson Paulino <anderson.paulino@ibm.com>
2023-09-12 22:22:22 -0300

    added jacoco and postgres

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS transactions (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    id BIGSERIAL PRIMARY KEY,
     amount DECIMAL(10, 2) NOT NULL,
     description VARCHAR(50),
     transaction_date DATE NOT NULL
